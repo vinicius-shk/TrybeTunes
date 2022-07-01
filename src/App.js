52c90a7b9519f0bc85d0352b8f5a59feeea9810c
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-07-01 14:51:41 -0300

    Conclusão do requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Search from './components/Search';
+import Album from './components/Album';
+import Favorites from './components/Favorites';
+import Profile from './components/Profile';
+import ProfileEdit from './components/ProfileEdit';
+import NotFound from './components/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+        <Switch>
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/search" component={ Search } />
+          <Route exact path="/" component={ Login } />
+          <Route exact path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/components/Album.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Album extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-album">teste div alb</div>
+    );
+  }
+}
+
+export default Album;
