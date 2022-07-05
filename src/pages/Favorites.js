1878a5452fb9df69dc15728e375cfd08e1f2a280
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-07-05 20:07:32 -0300

    Requisito 12 em progresso

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends React.Component {
+  constructor() {
+    super();
+    this.getFavourites = this.getFavourites.bind(this);
+    this.state = {
+      favorites: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getFavourites();
+  }
+
+  async getFavourites() {
+    const favorites = await getFavoriteSongs();
+    console.log(favorites, 'func');
+    this.setState({ favorites }, () => {
+    });
+  }
+
   render() {
+    const { favorites } = this.state;
+    console.log(favorites, 'render');
+    const element = favorites.map((song) => (
+      <MusicCard
+        key={ song.trackId }
+        musicObj={ song }
+        favouriteSongs={ favorites }
+      />));
     return (
       <div data-testid="page-favorites">
         <Header />
+        { element }
       </div>
     );
   }
