29764b93acbab4f496018ff4922d11276167511b
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-07-06 19:58:02 -0300

    Tentativa do requisito 12

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 import MusicCard from '../components/MusicCard';
 import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
@@ -7,7 +8,10 @@ class Favorites extends React.Component {
   constructor() {
     super();
     this.getFavourites = this.getFavourites.bind(this);
+    this.buildComponent = this.buildComponent.bind(this);
+    this.updateFavourites = this.updateFavourites.bind(this);
     this.state = {
+      isLoading: true,
       favorites: [],
     };
   }
@@ -16,27 +20,40 @@ class Favorites extends React.Component {
     this.getFavourites();
   }
 
+  // componentDidUpdate() {
+  //   this.updateFavourites();
+  // }
+
   async getFavourites() {
     const favorites = await getFavoriteSongs();
-    console.log(favorites, 'func');
-    this.setState({ favorites }, () => {
-    });
+    this.setState({ favorites, isLoading: false });
   }
 
-  render() {
+  updateFavourites() {
+    this.setState({ isLoading: true });
+    this.getFavourites();
+  }
+
+  buildComponent() {
     const { favorites } = this.state;
-    console.log(favorites, 'render');
     const element = favorites.map((song) => (
       <MusicCard
         key={ song.trackId }
         musicObj={ song }
         favouriteSongs={ favorites }
       />));
+    return element;
+  }
+
+  render() {
+    const { isLoading } = this.state;
     return (
-      <div data-testid="page-favorites">
+      <>
         <Header />
-        { element }
-      </div>
+        <div data-testid="page-favorites">
+          { isLoading ? <Loading /> : this.buildComponent() }
+        </div>
+      </>
     );
   }
 }
